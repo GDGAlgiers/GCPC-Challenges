a2339b0392ce6344423ba7c1a5498e83cc76b838
ymimene <ymimene@aas.com.sa>
2024-05-06 20:21:54 +0100

    New challenge added: Different One

---
+++ Different Ones/sol.py
@@ -0,0 +1,26 @@
+n, m, k = map(int, input().split())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a = set(a)
+b = set(b)
+
+cnt_a = 0
+cnt_b = 0
+stop = False
+
+for i in range(1, k + 1):
+    if i in a or i in b:
+        if i in a: 
+            cnt_a += 1
+        if i in b:
+            cnt_b += 1
+    else:
+        stop = True
+        break
+
+if cnt_a >= k // 2 and cnt_b >= k // 2 and not stop:
+    print('1')
+else:
+    print('0')
