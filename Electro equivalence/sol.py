014af66c840216b24747008bdcd37b3df3353365
Okba ALLAOUA <lo_allaoua@esi.dz>
2024-05-12 18:37:10 +0100

    Updated Electro equivalence description

--- Electro equivalence/sol.py
+++ Electro equivalence/sol.py
@@ -1,10 +1,7 @@
 def sol(net):
-    def serie(*args):
-        return sum(args)
-    def parallel(*args):
-        return 1 / sum((0 if (cur == 0) else (1/cur)) for cur in args)
-    def burned(*args):
-        return 0
+    def serie(*args): return sum(args)
+    def parallel(*args): return 1 / sum((0 if (cur == 0) else (1/cur)) for cur in args)
+    def burned(*args): return 0
 
     return eval(net.replace('(', 'serie(').replace('[', 'parallel(').replace('{', 'burned(').replace(']', ')').replace('}', ')'))
 
